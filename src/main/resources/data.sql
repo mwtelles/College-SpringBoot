19a4d05cf526951c7ed9d2836c42a23e9d42289e
mwtelles <mwtelles2012@hotmail.com>
2021-11-20 18:10:12 -0300

    Atualizando Banco para rodar a imagem

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,16 +15,13 @@ VALUES (4, 2, '90.90');
 
 
 INSERT INTO product(id, name, description, imageUrl, oldPrice, price, installments_id)
-VALUES (0, 'Leather Shoes Puma R698 Q4 V2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgur.com/a/i1SrNpF', '400.00', '350.00', 0);
+VALUES (1, 'Leather Shoes Puma R698 Q4 V2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://i.imgur.com/rQD2Z4p.png', '400.00', '350.00', 0);
 
 INSERT INTO product(id, name, description, imageUrl, oldPrice, price, installments_id)
-VALUES (1, 'Leather Shoes Nike R699 Q4 V2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgur.com/a/i1SrNpF', '399.00', '388.00', 1);
+VALUES (2, 'Leather Shoes Nike R699 Q4 V2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://i.imgur.com/rQD2Z4p.png', '399.00', '388.00', 1);
 
 INSERT INTO product(id, name, description, imageUrl, oldPrice, price, installments_id)
-VALUES (2, 'Leather Shoes Adidas R700 Q4 V2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgur.com/a/i1SrNpF', '379.00', '368.00', 2);
+VALUES (3, 'Leather Shoes Adidas R700 Q4 V2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://i.imgur.com/rQD2Z4p.png', '379.00', '368.00', 2);
 
 INSERT INTO product(id, name, description, imageUrl, oldPrice, price, installments_id)
-VALUES (3, 'Leather Shoes Vans R701 Q4 V2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgur.com/a/i1SrNpF', '359.00', '348.00', 3);
-
-INSERT INTO product(id, name, description, imageUrl, oldPrice, price, installments_id)
-VALUES (4, 'Leather Shoes Redley R702 Q4 V2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://imgur.com/a/i1SrNpF', '339.00', '328.00', 4);
+VALUES (4, 'Leather Shoes Vans R701 Q4 V2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'https://i.imgur.com/rQD2Z4p.png', '359.00', '348.00', 3);
